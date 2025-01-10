d94de7100f2a9f7603a1d40fde723c121e658ebe
alexandre1Work <alexandre.velosowork@gmail.com>
2025-01-10 11:45:21 -0300

    primeiras configurações (definindo tamanho do palco do jogo)

---
+++ script.js
@@ -0,0 +1,11 @@
+var largura = 0;
+var altura = 0;
+
+function ajustarTamanhoPalcoJogo() {
+    altura = window.innerHeight;
+    largura = window.innerWidth
+
+    console.log(altura, largura);
+}
+
+ajustarTamanhoPalcoJogo();
